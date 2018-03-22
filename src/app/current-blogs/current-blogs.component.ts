5a7732197e44c9d838ffd4e4b6b5890b7439adcc
Kelly Vaughan <kellyvaughan@Kellys-MacBook-Pro.local>
2018-03-21 22:08:23 -0400

    re-style blog post list

--- src/app/current-blogs/current-blogs.component.ts
+++ src/app/current-blogs/current-blogs.component.ts
@@ -9,25 +9,31 @@ export class CurrentBlogsComponent {
   title = 'app';
 
   blogs = [{
+      id: 1,
       month:"MAR",
       day:"06",
       year:"2018",
-      title: "Giving our bathroom some flair",
+      title: "Giving Our Bathroom Some Flair",
       cover: "bathroom.JPG",
+      location: "Durham, NC",
       content: "This bathroom has been one of the neglected rooms in our house since we moved in in August 2016. But after finding this old wooden shelf, I decided to finally put something together and finish this"
     },{
+      id: 2,
       month:"FEB",
       day:"27",
       year: "2018",
-      title: "Mudroom glam",
+      title: "Mudroom Glam",
       cover: "sign.JPG",
+      location: "Durham, NC",
       content: "This batse since we moved in in August 2016. But after finding this old wooden shelf, I decided to finally put something together and finish this"
     }, {
+      id:3,
       month:"AUG",
       day:"05",
       year: "2017",
-      title: "We finally have a dining room!",
+      title: "We Finally Have a Dining Room!",
       cover: "diningroom.JPG",
+      location: "Durham, NC",
       content: "This batse since we moved in in August 2016. But after finding this old wooden shelf, I decided to finally put something together and finish this"
    
     }];
